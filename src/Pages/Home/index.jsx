ba06a2df98cfb5334ad00f5b7991e2f27171641c
firmanasgani <firmanasgani37@gmail.com>
2024-11-07 20:30:35 +0700

    adding logo

--- src/Pages/Home/index.jsx
+++ src/Pages/Home/index.jsx
@@ -16,6 +16,27 @@ const Home = () => {
                 <input type="text" className="border-2 border-gray-400 rounded-md p-2 w-[80%]" placeholder="cari berdasarkan nama atau deskripsi" />
                 <button className="bg-red-400 text-white rounded-md p-2 w-[20%]">Cari</button>
             </div>
+            <div className="flex flex-row items-center justify-center gap-2 mt-10">
+                <div className="flex flex-row flex-nowrap gap-2 mt-5 overflow-x-auto scrollbar-none">
+                    {["Semua", "Indonesia", "Japan", "Korea", "China", "Thailand", "Vietnam", "Malaysia", "Singapore", "Philippines", "India"].map((country, i) => (
+                        <div key={i} className={`px-2 py-1 rounded ${i === 0 ? 'bg-green-500 text-white border-2 border-green-500' : ''}`}>
+                            {country}
+                        </div>
+                    ))}
+                </div>
+            </div>
+
+            <div className="flex flex-row  gap-2 mt-10">
+                <h1 className="text-2xl font-bold">
+                    Resep Baru
+                </h1>
+            </div>
+
+            <div className="flex flex-row  gap-2 mt-10">
+                <h1 className="text-2xl font-bold">
+                    Resep Populer
+                </h1>
+            </div>
 
 
         </div>
