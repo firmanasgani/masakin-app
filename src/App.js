a76eb1385a0304b2543325405a08242d90705dcc
Muhammad Quraisy <iye.fredickson@gmail.com>
2024-11-07 23:05:27 +0700

    feat(welcome): init welcome feature

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
-import { Route, Routes } from 'react-router-dom';
-import Home from './Pages/Home';
+import { Route, Routes } from "react-router-dom";
+import Home from "./Pages/Home";
+import Welcome from "./Pages/Welcome";
 
 function App() {
-
   return (
-      <Routes>
-        <Route path="/" element={<Home />} />
-      </Routes>
-  )
-
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/welcome" element={<Welcome />} />
+    </Routes>
+  );
 }
 
-export default App
+export default App;
