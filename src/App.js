fb5b68069b91192b5cef8297982ae545e75da1ac
LightKazuto <ilhamsidi62@gmail.com>
2024-11-11 19:45:49 +0700

    add detail-recipes route

--- src/App.js
+++ src/App.js
@@ -1,23 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+import { Routes, Route } from 'react-router-dom';
+import DetailRecipes from './Pages/DetailRecipes';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Routes>
+        <Route path="/detail-recipes" element={<DetailRecipes />} />
+      </Routes>
     </div>
   );
 }
