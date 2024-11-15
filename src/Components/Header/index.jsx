af2db0a33001de7093518a78471096c637be96c6
firmanasgani <firmanasgani37@gmail.com>
2024-11-15 22:30:36 +0700

    [ADDED] add new recipes on home

--- src/Components/Header/index.jsx
+++ src/Components/Header/index.jsx
@@ -2,8 +2,12 @@
 
 const Header = () => {
     return (
-        <div className="bg-blue-500 text-white py-4 px-6 text-center text-lg font-bold">
-            Masakin App
+        <div className="flex justify-between items-center bg-gray-200 py-2 px-6 text-sm">
+            <div className="flex items-center gap-2">
+                <span className="material-icons">signal_cellular_alt</span>
+                <span className="material-icons">battery_full</span>
+            </div>
+            <span>{new Date().toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' })}</span>
         </div>
     )
 }
--- src/Pages/Home/index.jsx
+++ src/Pages/Home/index.jsx
@@ -1,69 +1,98 @@
-import Layout from "../Layout"
+import { useState } from "react";
+import { SearchInput } from "../../Components/Inputs";
+import Layout from "../Layout";
+import CardNewRecipes from "../../Components/Cards/CardNewRecipes";
 
 const Home = () => {
-    return (
-      <Layout>
-          <div className="h-screen flex flex-col m-10">
-              <div className="flex flex-row items-center justify-between gap-2 mt-10">
-                <div className="text-2xl font-bold">Hello Indra</div>
-                <div className="w-10 h-10 bg-blue-500 rounded-full flex items-center justify-center">
-                    <span className="font-bold text-white text-2xl">I</span>
-                </div>
+  const [activeCategory, setActiveCategory] = useState("Semua");
 
-              </div>
-            <div className="text-md">Ingin rasa apa hari ini?</div>
-            <div className="flex flex-row items-center justify-center gap-2 mt-10">
-                <input type="text" className="border-2 border-gray-400 rounded-md p-2 w-[80%]" placeholder="cari berdasarkan nama atau deskripsi" />
-                <button className="bg-red-400 text-white rounded-md p-2 w-[20%]">Cari</button>
-            </div>
-            <div className="flex flex-row items-center justify-center gap-2 mt-10">
-                <div className="flex flex-row flex-nowrap gap-2 mt-5 overflow-x-auto scrollbar-none  no-scrollbar">
-                    {["Semua", "Indonesia", "Japan", "Korea", "China", "Thailand", "Vietnam", "Malaysia", "Singapore", "Philippines", "India"].map((country, i) => (
-                        <div key={i} className={`px-2 py-1 rounded ${i === 0 ? 'bg-green-500 text-white border-2 border-green-500' : ''}`}>
-                            {country}
-                        </div>
-                    ))}
-                </div>
-            </div>
-
-            <div className="flex flex-row  gap-2 mt-10">
-                <h1 className="text-xl font-bold">
-                    Resep Baru
-                </h1>
-                
-            </div>
-            <div className="flex flex-row flex-nowrap gap-2 mt-5 overflow-x-auto scrollbar-none  no-scrollbar">
-                {["Nasi Goreng", "Sushi", "Kimbab", "Pad Thai", "Soto"].map((food, i) => (
-                    <div key={i} className="flex flex-col bg-gray-300 justify-between gap-2 w-[250px] p-2 bg-white rounded-md shadow-md h-[250px]">
-                        <img src={`https://picsum.photos/200/300?random=${i}`} alt={food} className="rounded-t-md object-fit h-[100px]" />
-                        <div className="font-bold text-center text-sm">{food}</div>
-                        <div className="flex flex-row gap-1">
-                            {[1,2,3,4,5].map((star, j) => (
-                                <span key={j} className="text-yellow-500">&#9733;</span>
-                            ))}
-                        </div>
-                        <div className="mt-auto flex flex-row items-center justify-between">
-                            <span className="text-gray-500 text-sm">30 menit</span>
-                            <span className="text-gray-500">
-                                <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                                    <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M5 5a2 2 0 012-2h10a2 2 0 012 2v16l-7-3.5L5 21V5z" />
-                                </svg>
-                            </span>
-                        </div>
-                    </div>
-                ))}
-            </div>
-            <div className="flex flex-row  gap-2 mt-10">
-                <h1 className="text-xl font-bold">
-                    Resep Populer
-                </h1>
-            </div>
-
-
+  return (
+    <Layout>
+      <div className="h-screen flex flex-col m-10">
+        <div className="flex flex-row items-center justify-between gap-2 mt-10">
+          <div className="text-2xl font-bold">Hello Budi</div>
+          <div className="w-10 h-10 bg-blue-500 rounded-full flex items-center justify-center">
+            <span className="font-bold text-white text-2xl">B</span>
+          </div>
         </div>
-      </Layout>
-    )
-}
+        <div className="text-md">Ingin rasa apa hari ini?</div>
+        <SearchInput placeholder="cari berdasarkan nama atau deskripsi" />
+        <div className="flex flex-row items-center justify-center gap-2 mt-10">
+          <div className="flex flex-row flex-nowrap gap-2 mt-5 overflow-x-auto scrollbar-none  no-scrollbar">
+            {[
+              "Semua",
+              "Indonesia",
+              "Japan",
+              "Korea",
+              "China",
+              "Thailand",
+              "Vietnam",
+              "Malaysia",
+              "Singapore",
+              "Philippines",
+              "India",
+            ].map((country, i) => (
+              <div
+                key={i}
+                className={`px-2 py-1 rounded ${
+                  activeCategory === country
+                    ? "bg-green-500 text-white border-2 border-green-500"
+                    : ""
+                }`}
+                onClick={() => setActiveCategory(country)}
+              >
+                {country}
+              </div>
+            ))}
+          </div>
+        </div>
 
-export default Home
+        <div className="flex flex-row  gap-2 mt-10">
+          <h1 className="text-xl font-bold">Resep Baru</h1>
+        </div>
+        <div className="flex flex-col flex-wrap gap-2 mt-5 overflow-x-auto scrollbar-none  no-scrollbar h-[1200px]">
+          {["Soto Ayam Lamongan", "Sushi", "Kimbab", "Pad Thai", "Soto"].map(
+            (food, i) => (
+              <CardNewRecipes key={i} food={food} />
+            )
+          )}
+        </div>
+        <div className="flex flex-row  gap-2 mt-10">
+          <h1 className="text-xl font-bold">Resep Populer</h1>
+        </div>
+        <div className="flex flex-col flex-wrap gap-2 mt-5 overflow-x-auto scrollbar-none  no-scrollbar">
+          {["Soto Ayam Lamongan", "Sushi", "Kimbab", "Pad Thai", "Soto"].map(
+            (food, i) => (
+              <div
+                key={i}
+                className="relative w-[200px] h-[150px] border border-gray-300 rounded-md overflow-hidden"
+                style={{
+                  backgroundImage: `url(https://picsum.photos/200/300?random=${i})`,
+                  backgroundSize: "cover",
+                  backgroundPosition: "center",
+                }}
+              >
+                <div className="absolute inset-x-0 bottom-0 bg-gradient-to-t from-transparent to-black bg-opacity-50 p-2 flex flex-col gap-1 w-full">
+                <h1 className="text-xl font-bold text-white">{food}</h1>
+                  <div className="flex flex-row gap-1 text-sm">
+                    {[...Array(5)].map((_, index) => (
+                      <span key={index} className="text-yellow-500 text-xs material-icons">star</span>
+                    ))}
+                  </div>
+                  <div className="flex flex-row justify-between gap-1">
+                    <span className="text-white flex items-center gap-1">
+                      <span className="material-icons">access_time</span>
+                      30 min
+                    </span>
+                    <span className="text-white material-icons">bookmark</span>
+                  </div>
+              </div>
+              </div>
+            )
+          )}</div>
+      </div>
+    </Layout>
+  );
+};
 
+export default Home;
