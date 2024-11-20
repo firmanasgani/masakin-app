7eb29c34db537ac930df057d8801cd0e4d759f67
firmanasgani <firmanasgani37@gmail.com>
2024-11-21 02:16:32 +0700

    [MODIFIED] change header apps

--- src/Components/Header/index.jsx
+++ src/Components/Header/index.jsx
@@ -1,18 +1,7 @@
-
-setInterval(() => {
-    const date = new Date();
-    const time = date.toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' });
-    document.querySelector('.time').innerHTML = time;
-}, 1000);
-
 const Header = () => {
     return (
-        <div className="flex justify-between items-center bg-gray-200 py-2 px-6 text-sm">
-            <div className="flex items-center gap-2">
-                <span className="material-icons">signal_cellular_alt</span>
-                <span className="material-icons">battery_full</span>
-            </div>
-            <span className="time">{}</span>
+        <div className="flex justify-center items-center bg-blue-500 py-2 px-6 text-sm">
+            <h1 className="font-bold text-xl text-center text-white">Masakin</h1>
         </div>
     )
 }
